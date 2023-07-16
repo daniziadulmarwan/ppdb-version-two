ab705fe58493a2038b388a88a17775b34f8907b3
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-07-16 13:47:49 +0700

    add

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="{{$id}}">
+<div class="modal fade" id="{{ $id }}">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-body">
